3dff27ed8fe27dab3715af4569846e2ac6ce73b9
Mariaalonsoarribas <maria.alonsoarribas@edu.uah.es>
2025-06-12 16:03:06 +0200

    Arreglo pasajeros

--- .vscode/Trabajo/Pasajero.cpp
+++ .vscode/Trabajo/Pasajero.cpp
@@ -1,29 +1,32 @@
 #include "Pasajero.h"
+using namespace std;
 
 // Constructor por defecto
 Pasajero::Pasajero() 
-    : ID(0), minLlegada(0), minAtendido(0), duracionControl(0), pais(0), prioridad(0), IDBox(-1), salidaBox(0) {}
+    : ID(0), minLlegada(0), duracionControl(0), pais(""), prioridad(0), IDBox(-1), tiempoTotal(0) {}
 
 // Constructor con parámetros
-Pasajero::Pasajero(int ID, int minLlegada, int minAtendido, int duracionControl, int pais, int prioridad, int IDBox, int salidaBox) 
-    : ID(ID), minLlegada(minLlegada), minAtendido(minAtendido), duracionControl(duracionControl), pais(pais), prioridad(prioridad), IDBox(IDBox), salidaBox(salidaBox) {}
+Pasajero::Pasajero(int ID, int minLlegada, int duracionControl, string pais, int prioridad) 
+    : ID(ID), minLlegada(minLlegada), duracionControl(duracionControl), pais(pais), prioridad(prioridad), IDBox(-1), tiempoTotal(0) {}
 
 // Métodos de acceso
-int Pasajero::getID() const { return ID; }
-int Pasajero::getMinLlegada() const { return minLlegada; }
-int Pasajero::getMinAtendido() const { return minAtendido; }
-int Pasajero::getDuracionControl() const { return duracionControl; }
-int Pasajero::getPais() const { return pais; }
-int Pasajero::getPrioridad() const { return prioridad; }
-int Pasajero::getIDBox() const { return IDBox; }
-int Pasajero::getSalidaBox() const { return salidaBox; }
+int Pasajero::getID() { return ID; }
+int Pasajero::getMinLlegada() { return minLlegada; }
+int Pasajero::getDuracionControl() { return duracionControl; }
+string Pasajero::getPais() { return pais; }
+int Pasajero::getPrioridad()  { return prioridad; }
+int Pasajero::getTiempoTotal()  { return tiempoTotal; }
 
 // Métodos de modificación
-void Pasajero::setMinAtendido(int min) { minAtendido = min; }
 void Pasajero::setDuracionControl(int duracion) { duracionControl = duracion; }
-void Pasajero::setIDBox(int idBox) { IDBox = idBox; }
-void Pasajero::setSalidaBox(int salida) { salidaBox = salida; }
+void Pasajero::setID(int id) { ID = id; }
+void Pasajero::setMinLlegada(int min) { minLlegada = min; }
+void Pasajero::setDuracionControl(int duracion) { duracionControl = duracion; }
+void Pasajero::setPais(string p) { pais = p; }
 void Pasajero::setPrioridad(int p) { prioridad = p; }
+void Pasajero::setIDBox(int idBox) { IDBox = idBox; }
+void Pasajero::setTiempoTotal(int tiempo) { tiempoTotal = tiempo; }
+void Pasajero::setSalidaBox(int salida) { tiempoTotal = salida; } 
 
 // Destructor
 Pasajero::~Pasajero() {}
--- .vscode/Trabajo/Pasajero.h
+++ .vscode/Trabajo/Pasajero.h
@@ -1,42 +1,44 @@
-#ifndef PASAJERO_H
-#define PASAJERO_H
-#include <iostream>
+    #ifndef PASAJERO_H
+    #define PASAJERO_H
+    #include <iostream>
+    using namespace std;
 
-class Pasajero {
-private:
-    int ID;
-    int minLlegada; //Minuto de llegada al aeropuerto.
-    int minAtendido; //Minuto en el que comienza a ser atendido en el box.
-    int duracionControl; //Duración del control de aduanas.
-    int pais;
-    int prioridad;
-    int IDBox;
-    int salidaBox; //Minuto en el que sale del box, una vez atendido
-
-
-public:
-    Pasajero(); // Declaración del constructor por defecto
-    Pasajero(int ID, int minLlegada, int minAtendido, int duracionControl, int pais, int prioridad, int IDBox, int salidaBox); // Constructor con parámetros
-
-    // Métodos de acceso
-    int getID() const;
-    int getMinLlegada() const;
-    int getMinAtendido() const;
-    int getDuracionControl() const;
-    int getPais() const;
-    int getPrioridad() const;
-    int getIDBox() const;
-    int getSalidaBox() const;
+    class Pasajero {
+    private:
+        int ID;
+        int minLlegada; //Minuto de llegada al aeropuerto.
+        int duracionControl; //Duración del control de aduanas.
+        string pais;
+        int prioridad;
+        int IDBox;
+        int tiempoTotal; //Minuto en el que sale del box, una vez atendido
 
 
-    // Métodos de modificación
-    void setMinAtendido(int min);
-    void setDuracionControl(int duracion);
-    void setIDBox(int idBox);
-    void setSalidaBox(int salida);
-    void setPrioridad(int prioridad);
+    public:
+        Pasajero(); // Declaración del constructor por defecto
+        Pasajero(int ID, int minLlegada, int duracionControl, string pais, int prioridad); // Constructor con parámetros
 
-    ~Pasajero(); // Destructor
-};
+        // Métodos de acceso
+        int getID();
+        int getMinLlegada();
+        int getDuracionControl();
+        string getPais();
+        int getPrioridad();
+        int getTiempoTotal();
 
-#endif // PASAJERO_H
+
+        // Métodos de modificación
+        void setID(int id);
+        void setMinLlegada(int min);
+        void setDuracionControl(int duracion);
+        void setPais(string pais);
+        void setPrioridad(int prioridad);
+        void setSalidaBox(int salida);
+        void setIDBox(int IDBox);
+        void setPrioridad(int prioridad);
+        void setTiempoTotal(int tiempo);
+
+        ~Pasajero(); // Destructor
+    };
+
+    #endif // PASAJERO_H
